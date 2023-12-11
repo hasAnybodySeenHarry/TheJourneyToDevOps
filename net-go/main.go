7c673106cd482913143999c451dfddc3f1cc166c
hasAnybodySeenHarry <harryyan.info@proton.me>
2023-12-11 15:11:05 +0630

    Modified to render template.html

--- net-go/main.go
+++ net-go/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<h1>The Journey to DevOps - Go</h1>")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w)
+	})
 
-    port := 3000
-    fmt.Printf("Server is running on :%d...\n", port)
-    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	port := 3000
+	fmt.Printf("Server is running on :%d...\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
+func renderTemplate(w http.ResponseWriter) {
+	tmplFile := "template.html"
+	tmpl, err := template.ParseFiles(tmplFile)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
