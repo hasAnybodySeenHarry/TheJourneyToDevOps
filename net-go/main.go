f87519d6bf026be75e66352cc292261856facc08
hasAnybodySeenHarry <harryyan.info@proton.me>
2023-12-11 15:20:23 +0630

    Fixed template err

--- net-go/main.go
+++ net-go/main.go
@@ -20,13 +20,13 @@ func renderTemplate(w http.ResponseWriter) {
 	tmplFile := "template.html"
 	tmpl, err := template.ParseFiles(tmplFile)
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.Execute(w, nil)
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, err, http.StatusInternalServerError)
 		return
 	}
 }
